8a3204b70b62f08722ad850ee54679cf81cdfe9c
Les Orchard <me@lmorchard.com>
2020-06-30 22:14:24 -0700

    Updates

---
+++ index.js
@@ -0,0 +1,104 @@
+const LAZY_LOAD_THRESHOLD = 0.1;
+
+async function main() {
+  initLazyLoadObserver();
+  initEventDelegation();
+  pageChanged();
+
+  console.log("READY.");
+}
+
+function pageChanged() {
+  applyTimeAgo();
+  updateLazyLoadObserver();
+}
+
+function applyTimeAgo() {
+  timeago.cancel();
+  timeago.render(document.querySelectorAll(".timeago"), "en_US", {
+    minInterval: 10,
+  });
+}
+
+function initEventDelegation() {
+  document.body.addEventListener("click", async (ev) => {
+    const target = ev.target;
+    if (/a/i.test(target.tagName) && target.classList.contains("load-href")) {
+      handleLoaderClick(ev, target);
+    }
+  });
+}
+
+async function handleLoaderClick(ev, target) {
+  ev.preventDefault();
+  await replaceElementWithHTMLResource(
+    target.parentNode,
+    target.getAttribute("href")
+  );
+}
+
+async function replaceElementWithHTMLResource(element, href) {
+  if (element.classList.contains("loading")) { return; }
+  element.classList.add("loading");
+  element.setAttribute("disabled", true);
+
+  const response = await fetch(href);
+  const content = await response.text();
+
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(content, "text/html");
+  const loadedNodes = Array.from(doc.body.children);
+
+  const parent = element.parentNode;
+  for (const node of loadedNodes) {
+    parent.insertBefore(document.adoptNode(node), element);
+  }
+
+  element.remove();
+  pageChanged();
+}
+
+async function handleLazyLoadIntersection({ target }) {
+  if (/img/i.test(target.tagName)) {
+    const src = target.getAttribute("data-src");
+    if (src) {
+      target.setAttribute("src", src);
+      target.removeAttribute("data-src");
+    }
+  }
+
+  if (target.classList.contains("load-href")) {
+    await replaceElementWithHTMLResource(
+      target.parentNode,
+      target.getAttribute("href")
+    );
+  }
+}
+
+let lazyLoadObserver;
+function initLazyLoadObserver() {
+  lazyLoadObserver = new IntersectionObserver(
+    (entries) => {
+      for (const entry of entries) {
+        if (entry.isIntersecting) {
+          handleLazyLoadIntersection(entry);
+        }
+      }
+    },
+    {
+      threshold: LAZY_LOAD_THRESHOLD,
+    }
+  );
+}
+
+function updateLazyLoadObserver() {
+  lazyLoadObserver.disconnect();
+  const toObserve = document.querySelectorAll(".lazy-load");
+  for (const element of toObserve) {
+    lazyLoadObserver.observe(element);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  main().catch((err) => console.error(err));
+});
