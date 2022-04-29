9a33a06f464b099437cc1074c7e72f07c990f43d
Les Orchard <lmorchard@github.com>
2022-04-29 16:57:18 -0700

    get indexes and partition updates working

--- cli/play.js
+++ cli/play.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import PQueue from "p-queue";
 import { MetaPriorityQueue } from "../lib/queue.js";
-import { Feed } from "../lib/models.js";
+import { FeedSet, Feed } from "../lib/models.js";
 
 export default function (init, program) {
   program
@@ -25,55 +25,10 @@ async function command(options, command, context) {
   console.log(feed);
   */
 
-  const data = fs.readFileSync("data/feeds.txt", "UTF-8");
-  const lines = data.split(/\r?\n/).filter((line) => !!line);
-
-  const fetchQueue = new PQueue({
-    concurrency: config.feedPollConcurrency,
-    /*
-    queueClass: MetaPriorityQueue({
-      onAdd: (meta) => {
-        log.debug("Fetch queue add %s", JSON.stringify(meta));
-      },
-      onRun: (meta) => {
-        log.debug("Fetch queue run %s", JSON.stringify(meta));
-      },
-      onResolved: (meta) => {
-        log.debug("Fetch queue resolved %s", JSON.stringify(meta));
-      },
-    }),
-    */
-  });
-
-  const timeStart = Date.now();
-
-  const queueStatusTimer = setInterval(() => {
-    log.info(
-      "Fetch queue status (%s / %s)",
-      fetchQueue.pending,
-      fetchQueue.size
-    );
-  }, 1000);
-
-  const queueFetch = (url) =>
-    fetchQueue.add(
-      async () => {
-        log.info(`Fetching ${url}`);
-        try {
-          await new Feed(url, config, log).poll();
-        } catch (e) {
-          log.error("Unexpected error");
-          log.error(e);
-        }
-      },
-      { meta: { url } }
-    );
-
-  await Promise.all(lines.map(queueFetch));
-
-  log.info("Feed polling complete. (%sms)", Date.now() - timeStart);
-
-  clearInterval(queueStatusTimer);
+  const feedSet = new FeedSet(config, log);
+  await feedSet.load();
+  await feedSet.loadFromTxt("data/feeds.txt");
+  await feedSet.poll();
 }
 
 export const parseOpmlStream = ({ stream }) =>
